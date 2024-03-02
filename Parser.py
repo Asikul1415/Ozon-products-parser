1bb62402f3468f0af665f0740e1e2857f361e16d
Asikul1415 <asikulsteam@yandex.ru>
2024-03-02 22:58:02 +0500

    Had a push problem. Now this what I've changed: 1) Added processing of the catalog in the form of product cards, rather than a list of products 2) Fixed a bug in which the computer could freeze or the page would not load due to RAM overload. 3) Fixed a bug where the name of the output file was duplicated 4) Added a parameter indicating which page to start parsing from

--- Parser.py
+++ Parser.py
@@ -2,23 +2,33 @@ import time
 import os
 from bs4 import BeautifulSoup as bs 
 import undetected_chromedriver as uc
+from undetected_chromedriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 import json
+import re
+import psutil
 
 
-class OzonParser() :
-    driver = uc.Chrome()
+class OzonParser() : 
     pages = []
     links_to_products = []
     products = []
     path_to_directory = os.path.abspath(__file__).replace(os.path.basename(__file__),'')
     
-    def parse_pages(self,pages_count):
-        i = 1
+    def parse_pages(self,begining_page,pages_count):
+        i = begining_page 
+        temp = i
         url = self.url
-        if(url.count('?') == 1): url.join(f'&page={i}')
-        else: url.join(f'/?page={i}')
+        if(url.count('?') == 1): 
+            url += f'&page={i}'
+        else: 
+            url += f'/?page={i}'
 
-        while url and i <= pages_count:
+        while url and i < temp + pages_count:
             print(f'\n\n\n\nСтраница №{i}')
 
             self.driver.get(url)
@@ -30,49 +40,82 @@ class OzonParser() :
             self.pages.append(html_code)
             
             i += 1
-            url_temp = html_code.find('a', {'class' : 'ep6 b235-a0 b235-b6 b235-b1'})
-            if(url_temp != None): url = 'https://www.ozon.ru' + url_temp['href']
-            else: url = None
+            
+            #Checking if next page exist
+            url_temp = html_code.find('a', {'class' : 'p6e b237-a0 b237-b6 b237-b1'}) 
+            if(url_temp != None): 
+                url = 'https://www.ozon.ru' + url_temp['href']
+            else: 
+                url = None
 
         
     def parse_links_to_products(self):
         os.system('cls')
         for page in self.pages:
-            divs = page.find_all('div', {'class' : 'ix0'})
+            divs = page.find_all('div', {'class' : 'xi1'})
+            if(divs == []): 
+                divs = page.find_all('div', {'class' : 'wi7'})
+
             for div in divs:
-                a = div.find('a', {'class' : 'tile-hover-target it2 ti2'})
+                a = div.find('a', {'class' : 'tile-hover-target it3 ti3'})
                 href = a['href']
                 self.parse_product(href.split('?')[0])
 
     def parse_product(self,href):
         url = 'https://www.ozon.ru' + href
-        self.driver.get(url = url)
+        product = {}
+        
+        print({f"\n{url}"})
 
-        time.sleep(4)
+        memory_percent = psutil.virtual_memory().percent
+
+        if(memory_percent >= 75): 
+            self.driver.quit()
+            self.driver = uc.Chrome(driver_executable_path=self.driver_path)
+            
+        try:
+            self.driver.get(url = url)
+            wait = WebDriverWait(self.driver, 20)
+            wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*")))   
+        except TimeoutException:
+            self.driver.quit()
+            self.driver = uc.Chrome(driver_executable_path=self.driver_path)
+            self.driver.get(url = url)
+ 
+        time.sleep(4) 
 
         html_code = bs(self.driver.page_source,'lxml')
-        product = {}
-        print(html_code)
+        
+        if(html_code.find('h2', {'class' : 'zk4'}) != None): 
+            return #Checking if the product is not out of stock
+               
 
-        temp = html_code.find('h1', {'class' : 'pl9'})
+        temp = html_code.find('h1', {'class' : 'lq1'})
         name_of_product = ''
 
         if(temp != None): name_of_product = temp.text
         else: name_of_product = 'Не найдено'
 
-        price_temp = html_code.find('span', {'class' : 'l4p pl4 l8p'})
-        if(price_temp is None): price_temp = html_code.find('span', {'class' : 'l4p pl4 p8l'})
-        if(price_temp is None): price_temp = html_code.find( 'span' ,{'class ': 'lp l8o'})
-        price_with_discount = price_temp.text
 
-        price_temp = html_code.find('span', {'class' : 'p3l p4l p2l lp4'})
+        price_temp = html_code.find('span', {'class' : 'l6p pl6 ql'})
+        if(price_temp is None): 
+            price_temp = html_code.find('span', {'class' : 'pl1 pl'})
+        if(price_temp is None): 
+            price_temp = html_code.find( 'span' ,{'class': 'l6p pl6 l0q'})
+        price_with_discount = price_temp.text
+    
+
+        price_temp = html_code.find('span', {'class' : 'p5l p6l p4l lp6'})
         if(price_temp is not None): price_without_discount = price_temp.text
         else: price_without_discount = 'Нет'
 
-        rating_temp = html_code.find('div', {'class' : 'vr6'})
+
+        rating_temp = html_code.find('div', {'class' : 'rv9'})
         if(rating_temp is None): rating_temp = 'Нет' 
         else: rating_temp = rating_temp.find('span').text
         rating = rating_temp
+
+
 
         product['Название'] = name_of_product
         product['Цена со скидкой'] = price_with_discount
@@ -80,12 +123,13 @@ class OzonParser() :
         product['Рейтинг'] = rating
         product['Ссылка'] = url
         
-        name_of_characteristics = html_code.find_all('dt', {'class' : 'j3v'})
-        characteristics = html_code.find_all('dd', {'class' : 'vj3'})
+        
+        name_of_characteristics = html_code.find_all('dt', {'class' : 'j5v'})
+        characteristics = html_code.find_all('dd', {'class' : 'vj5'})
         
 
         for i in range(len(characteristics)):
-            name_of_characteristic_temp = name_of_characteristics[i].find('span', {'class' : 'v3j'})
+            name_of_characteristic_temp = name_of_characteristics[i]
 
             if(name_of_characteristic_temp is None): 
                 name_of_characteristic = name_of_characteristics[i].text
@@ -111,8 +155,8 @@ class OzonParser() :
         self.driver.quit()
         self.driver.service.stop()
 
-    def parse(self,pages_count):
-        self.parse_pages(pages_count)
+    def parse(self,pages_count,begining_page=1):
+        self.parse_pages(begining_page,pages_count)
         self.parse_links_to_products()
         self.exit()
         print('Было спарсено: '  + str(len(self.products)))
@@ -123,15 +167,15 @@ class OzonParser() :
         keys = product.keys()
         for key in keys:
             print(key + ': ' + product[key])
-    
+
     def convertToJsonFile(self,file_name='products.json',path=''):
         if(path == ''): path = self.path_to_directory + file_name
+        elif(path != ''): path = path + file_name
 
         with open(path,'w',encoding='utf-8') as json_file:
             json.dump(self.products, json_file,indent = 2, ensure_ascii= False)
     
-    def __init__(self,url):
+    def __init__(self,url,driver_path = None):
         self.url = url
-
-
-
+        self.driver_path = driver_path
+        self.driver = uc.Chrome(driver_executable_path=self.driver_path)
