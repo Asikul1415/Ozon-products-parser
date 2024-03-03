e174ea4f5dc91370aee0a0a52759bbc3f0aa83c4
Asikul1415 <asikulsteam@yandex.ru>
2024-03-03 12:34:43 +0500

    I've broken the parse_product method into smaller ones, for ease of understanding and unloading the method code.

--- Parser.py
+++ Parser.py
@@ -1,6 +1,7 @@
 import time
 import os
 from bs4 import BeautifulSoup as bs 
+
 import undetected_chromedriver as uc
 from undetected_chromedriver import Chrome
 from selenium.webdriver.common.by import By
@@ -19,6 +20,7 @@ class OzonParser() :
     products = []
     path_to_directory = os.path.abspath(__file__).replace(os.path.basename(__file__),'')
     
+
     def parse_pages(self,begining_page,pages_count):
         i = begining_page 
         temp = i
@@ -48,6 +50,7 @@ class OzonParser() :
             else: 
                 url = None
 
+
         
     def parse_links_to_products(self):
         os.system('cls')
@@ -60,6 +63,8 @@ class OzonParser() :
                 a = div.find('a', {'class' : 'tile-hover-target it3 ti3'})
                 href = a['href']
                 self.parse_product(href.split('?')[0])
+
+
 
     def parse_product(self,href):
         url = 'https://www.ozon.ru' + href
@@ -86,17 +91,23 @@ class OzonParser() :
 
         html_code = bs(self.driver.page_source,'lxml')
         
+        #Checking if the product is not out of stock
         if(html_code.find('h2', {'class' : 'zk4'}) != None): 
-            return #Checking if the product is not out of stock
+            return 
                
+        price_temp = self.parse_price(html_code= html_code)
 
-        temp = html_code.find('h1', {'class' : 'lq1'})
-        name_of_product = ''
+        product['Название'] = self.parse_name(html_code= html_code)
+        product['Цена со скидкой'] = price_temp[0] #price_with_discount
+        product['Цена без скидки'] = price_temp[1] #price_without_discount
+        product['Рейтинг'] = self.parse_rating(html_code= html_code)
+        product['Ссылка'] = url
+        self.parse_charasterics(html_code=html_code, product=product)
 
-        if(temp != None): name_of_product = temp.text
-        else: name_of_product = 'Не найдено'
+        
+        self.products.append(product)
 
-
+    def parse_price(self,html_code : str):
         price_temp = html_code.find('span', {'class' : 'l6p pl6 ql'})
         if(price_temp is None): 
             price_temp = html_code.find('span', {'class' : 'pl1 pl'})
@@ -106,24 +117,35 @@ class OzonParser() :
     
 
         price_temp = html_code.find('span', {'class' : 'p5l p6l p4l lp6'})
-        if(price_temp is not None): price_without_discount = price_temp.text
+
+        if(price_temp is not None): 
+            price_without_discount = price_temp.text
         else: price_without_discount = 'Нет'
 
-
+        return tuple[price_with_discount,price_without_discount]
+    
+    def parse_rating(self,html_code: str):
         rating_temp = html_code.find('div', {'class' : 'rv9'})
-        if(rating_temp is None): rating_temp = 'Нет' 
-        else: rating_temp = rating_temp.find('span').text
-        rating = rating_temp
 
+        if(rating_temp is None): 
+            rating_temp = 'Нет' 
+        else: 
+            rating_temp = rating_temp.find('span').text
 
+        return rating_temp
 
-        product['Название'] = name_of_product
-        product['Цена со скидкой'] = price_with_discount
-        product['Цена без скидки'] = price_without_discount
-        product['Рейтинг'] = rating
-        product['Ссылка'] = url
+    def parse_name(self, html_code: str):   
+        temp = html_code.find('h1', {'class' : 'lq1'})
+        name_of_product = ''
+
+        if(temp != None): 
+            name_of_product = temp.text
+        else: 
+            name_of_product = 'Не найдено'
         
-        
+        return name_of_product
+
+    def parse_charasterics(self,html_code: str,product):
         name_of_characteristics = html_code.find_all('dt', {'class' : 'j5v'})
         characteristics = html_code.find_all('dd', {'class' : 'vj5'})
         
@@ -136,17 +158,20 @@ class OzonParser() :
             else:
                 name_of_characteristic = name_of_characteristic_temp.text  
 
-            if(i < len(name_of_characteristics) - 1 == False): continue
+            if(i < len(name_of_characteristics) - 1 == False): 
+                continue
             
             if(name_of_characteristic not in product.keys()):
                 product[f'{name_of_characteristic}'] = f'{characteristics[i].text}'          
 
-        self.products.append(product)
     
+
     def exit(self):
         time.sleep(2)
         self.driver.quit()
         self.driver.service.stop()
+
+
 
     def parse(self,pages_count,begining_page=1):
         self.parse_pages(begining_page,pages_count)
@@ -154,6 +179,8 @@ class OzonParser() :
         self.exit()
         print('Было спарсено: '  + str(len(self.products)))
         return self.products
+    
+
 
     def printProduct(self,index):
         product = self.products[index]
@@ -161,12 +188,16 @@ class OzonParser() :
         for key in keys:
             print(key + ': ' + product[key])
 
+
+
     def convertToJsonFile(self,file_name='products.json',path=''):
         if(path == ''): path = self.path_to_directory + file_name
         elif(path != ''): path = path + file_name
 
         with open(path,'w',encoding='utf-8') as json_file:
             json.dump(self.products, json_file,indent = 2, ensure_ascii= False)
+
+
     
     def __init__(self,url,driver_path = None):
         self.url = url
