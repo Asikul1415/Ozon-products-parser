3cea8aa16f2aea4771c85078820b36832ac5c493
Asikul1415 <asikulsteam@yandex.ru>
2024-04-03 20:53:00 +0500

    I rewrote the code so that I didn’t have to change the names of all classes again every 2 days. I removed the Beautiful Soup module as unnecessary. Now all parsing occurs on Selenium.

--- Parser.py
+++ Parser.py
@@ -1,171 +1,167 @@
 import time
 import os
-from bs4 import BeautifulSoup as bs 
 
 import undetected_chromedriver as uc
-from undetected_chromedriver import Chrome
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 
 import json
 import psutil
 
 
 class OzonParser() : 
-    __pages = []
     __products = []
+    __links_to_products = []
     __path_to_directory = os.path.abspath(__file__).replace(os.path.basename(__file__),'')
     
 
+    def __init__(self,url: str,driver_path: str = None):
+        self.url = url
+        self.driver_path = driver_path
+        self.driver = uc.Chrome(driver_executable_path=self.driver_path)
+
+
+
     def __parse_pages(self,begining_page,pages_count):
-        i = begining_page 
-        temp = i
+        page = begining_page 
+        temp = page
         url = self.url
         if(url.count('?') == 1): 
-            url += f'&page={i}'
+            url += f'&page={page}'
         else: 
-            url += f'/?page={i}'
+            url += f'/?page={page}'
 
-        while url and i < temp + pages_count:
-            print(f'\n\n\n\nСтраница №{i}')
+        while page < temp + pages_count:
+            print(f'\n\n\n\nСтраница №{page}')
 
             self.driver.get(url)
 
-            time.sleep(8)
+            scroll_position = 0
+            for i in range(8):
+                scroll_position += 400
+                self.driver.execute_script(f"window.scrollTo(0, {scroll_position});")
+                time.sleep(1)
 
-            html_code = bs(self.driver.page_source,'lxml')
-            
-            self.__pages.append(html_code)
-            
-            i += 1
+            page += 1
+
+            self.__parse_links_to_products()
+
             
             #Checking if next page exist
-            url_temp = html_code.find('a', {'class' : 'e7p b239-a0 b239-b6 b239-b1'}) 
-            if(url_temp != None): 
-                url = 'https://www.ozon.ru' + url_temp['href']
-            else: 
-                url = None
+            url_temp = self.driver.find_elements(By.CSS_SELECTOR, "[class$='tsBodyControl400Small']")
+            if(url_temp != [] ):
+                grapefruit_count = 0
+                for _temp in url_temp:
+                    if( _temp.text != 'Дальше'):
+                        grapefruit_count += 1
+                if(grapefruit_count != len(url_temp)):
+                    url = url.replace(f'page={page-1}',f'page={page}')
+                else:
+                    break
 
 
         
-    def __parse_links_to_products(self):
-        os.system('cls')
-        for page in self.__pages:
-            divs = page.find_all('div', {'class' : 'i3w'})
-            if(divs == []): 
-                divs = page.find_all('div', {'class' : 'wi'})
-
-            for div in divs:
-                a = div.find('a', {'class' : 'tile-hover-target ai8 ia8'})
-                href = a['href']
-                self.__parse_product(href.split('?')[0])
+    def __parse_links_to_products(self) -> None:
+        urls = self.driver.find_elements(By.CSS_SELECTOR,"[class^='tile-hover-target']")
+        for url in urls:
+            href = url.get_attribute("href")
+            self.__links_to_products.append(href.split('?')[0])
 
 
 
-    def __parse_product(self,href):
-        url = 'https://www.ozon.ru' + href
-        product = {}
+    def __parse_products(self):
         
-        print({f"\n{url}"})
-
-        memory_percent = psutil.virtual_memory().percent
-
-        if(memory_percent >= 75): 
-            self.driver.quit()
-            self.driver = uc.Chrome(driver_executable_path=self.driver_path)
+        for url in self.__links_to_products:      
+            product = {}
             
-        try:
-            self.driver.get(url = url)
-            wait = WebDriverWait(self.driver, 20)
-            wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*")))   
-        except TimeoutException:
-            self.driver.quit()
-            self.driver = uc.Chrome(driver_executable_path=self.driver_path)
-            self.driver.get(url = url)
- 
-        time.sleep(4) 
+            print({f"\n{url}"})
 
-        html_code = bs(self.driver.page_source,'lxml')
+            memory_percent = psutil.virtual_memory().percent
+
+            if(memory_percent >= 75): 
+                self.driver.quit()
+                self.driver = uc.Chrome(driver_executable_path=self.driver_path)
+                
+            try:
+                self.driver.get(url = url)
+            except TimeoutException:
+                self.driver.quit()
+                self.driver = uc.Chrome(driver_executable_path=self.driver_path)
+                self.driver.get(url = url)
+                time.sleep(3)
+            
+
+            scroll_position = 0
+
+            # Прокручиваем страницу до последнего элемента
+            for i in range(4):
+                scroll_position += 500
+                self.driver.execute_script(f"window.scrollTo(0, {scroll_position});")
+                time.sleep(1)
+            
+            try:    
+                price_temp = self.__parse_price()
+
+                product['Название'] = self.__parse_name()
+                product['Цена со скидкой'] = price_temp[0] #price_with_discount
+                product['Цена без скидки'] = price_temp[1] #price_without_discount
+                product['Рейтинг'] = self.__parse_rating()
+                product['Ссылка'] = url
+                self.__parse_charasterics(product = product)
+
+                self.__products.append(product)
+            except NoSuchElementException:
+                print("\n\nТовар кончился\n\n")
+                return
+
+
+    def __parse_price(self) -> tuple[str,str]:
+        prices = []
+
+        webPrice = self.driver.find_element(By.CSS_SELECTOR,"div[data-widget='webPrice']")
+        class_temp = webPrice.get_attribute("class").split()[0]
+        class_temp_temp = self.driver.find_element(By.CSS_SELECTOR,f".{class_temp}")
+        prices_temp = class_temp_temp.text.split('\n')
+
+        for price in prices_temp:
+            if('₽' in price):
+                prices.append(price)
+
+        price_without_discount = prices[-1]
+        if(len(prices) == 3):
+            price_with_discount = prices[1]
+        elif(len(prices) == 2):
+            price_with_discount = prices[0]
+        else:
+            price_with_discount = 'Нету'
         
-        #Checking if the product is not out of stock
-        if(html_code.find('h2', {'class' : 'ky6'}) != None): 
-            return 
-               
-        price_temp = self.__parse_price(html_code= html_code)
-
-        product['Название'] = self.__parse_name(html_code= html_code)
-        product['Цена со скидкой'] = price_temp[0] #price_with_discount
-        product['Цена без скидки'] = price_temp[1] #price_without_discount
-        product['Рейтинг'] = self.__parse_rating(html_code= html_code)
-        product['Ссылка'] = url
-        self.__parse_charasterics(html_code=html_code, product=product)
-
-        
-        self.__products.append(product)
-
-    def __parse_price(self,html_code : bs) -> tuple[str,str]:
-        price_temp = html_code.find('span', {'class' : 'lo9 l9o pl2'})
-        if(price_temp is None): 
-            price_temp = html_code.find('span', {'class' : 'l4o ol2'})
-        if(price_temp is None): 
-            price_temp = html_code.find( 'span' ,{'class': 'lo9 l9o lp3'}) 
-        price_with_discount = price_temp.text 
-    
-
-        price_temp = html_code.find('span', {'class' : 'ol8 ol9 ol7 o8l'})
-
-        if(price_temp is not None): 
-            price_without_discount = price_temp.text
-        else: price_without_discount = 'Нет'
-
         return (price_with_discount,price_without_discount)
-    
-    def __parse_rating(self,html_code: bs):
-        rating_temp = html_code.find('div', {'class' : 'rw7'})
+         
+         
+    def __parse_rating(self):
+        columns = self.driver.find_elements(By.CSS_SELECTOR,"div[data-widget='column']")
+        for column in columns:
+            if( '/ 5' in column.text ):
+                return column.text.split('\n')[0]
 
-        if(rating_temp is None): 
-            rating_temp = 'Нет' 
-        else: 
-            rating_temp = rating_temp.find('span').text
-
-        return rating_temp
-
-    def __parse_name(self, html_code: bs):   
-        temp = html_code.find('h1', {'class' : 'p3l'})
-        name_of_product = ''
-
-        if (temp == None):
-            temp = html_code.find('div',{'class' : 'pl5 p5l'})
-        if(temp == None):
-            name_of_product = 'Не найдено'
-    
-        if(temp != None): 
-            name_of_product = temp.text
+    def __parse_name(self):   
+        name_temp = self.driver.find_element(By.CSS_SELECTOR,"div[data-widget='webProductHeading']")
         
-        return name_of_product
+        return name_temp.text
 
-    def __parse_charasterics(self,html_code: bs,product):
-        name_of_characteristics = html_code.find_all('dt', {'class' : 'u6j'})
-        characteristics = html_code.find_all('dd', {'class' : 'ju7'})
-        
+    def __parse_charasterics(self,product):
+        section_characteristics = self.driver.find_element(By.CSS_SELECTOR,"div[id='section-characteristics']")
+        names_of_characteristics = section_characteristics.find_elements(By.CSS_SELECTOR, "dt")
+        characteristics = section_characteristics.find_elements(By.CSS_SELECTOR, "dd")
 
         for i in range(len(characteristics)):
-            name_of_characteristic_temp = name_of_characteristics[i]
+            characteristic = characteristics[i].text
+            name_of_characteristic = names_of_characteristics[i].text
 
-            if(name_of_characteristic_temp is None): 
-                name_of_characteristic = name_of_characteristics[i].text
-            else:
-                name_of_characteristic = name_of_characteristic_temp.text  
-
-            if(i < len(name_of_characteristics) - 1 == False): 
-                continue
-            
             if(name_of_characteristic not in product.keys()):
-                product[f'{name_of_characteristic}'] = f'{characteristics[i].text}'          
+                product[name_of_characteristic] = characteristic
 
-    
 
     def __exit(self):
         time.sleep(2)
@@ -173,22 +169,19 @@ class OzonParser() :
         self.driver.service.stop()
 
 
-
     def parse(self,pages_count,begining_page=1):
         self.__parse_pages(begining_page,pages_count)
-        self.__parse_links_to_products()
+        self.__parse_products()
         self.__exit()
         print('Было спарсено: '  + str(len(self.__products)))
         return self.__products
     
-
 
     def printProduct(self,index):
         product = self.__products[index]
         keys = product.keys()
         for key in keys:
             print(key + ': ' + product[key])
-
 
 
     def convertToJsonFile(self,file_name='products.json',path=''):
@@ -198,10 +191,4 @@ class OzonParser() :
         with open(path,'w',encoding='utf-8') as json_file:
             json.dump(self.__products, json_file,indent = 2, ensure_ascii= False)
 
-
-    
-    def __init__(self,url,driver_path = None):
-        self.url = url
-        self.driver_path = driver_path
-        self.driver = uc.Chrome(driver_executable_path=self.driver_path)
 
